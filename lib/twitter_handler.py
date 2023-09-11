804210c46ee07f86d2f4b4d034cae6de30dc087c
TheGreatCodeHolio <ian@icarey.net>
2023-09-10 21:59:01 -0400

    commented this file

--- lib/twitter_handler.py
+++ lib/twitter_handler.py
@@ -1,14 +1,20 @@
 import tweepy
 
+
 def format_tweet():
+    pass
+
+
 def post_tweet(config_data, detector_data, call_data, mp3_url):
-    client = tweepy.Client(consumer_key=config_data["twitter_settings"]["consumer_key"],
-                           consumer_secret=config_data["twitter_settings"]["consumer_secret"],
-                           access_token=config_data["twitter_settings"]["access_token"],
-                           access_token_secret=config_data["twitter_settings"]["access_token_secret"])
-    try:
-        tweet_response = client.create_tweet(text=tweet_text)
-        return tweet_response.data
-    except tweepy.TweepyException as e:
-        print(f"Error posting tweet: {e}")
-        return None
+    return None
+
+    # client = tweepy.Client(consumer_key=config_data["twitter_settings"]["consumer_key"],
+    #                        consumer_secret=config_data["twitter_settings"]["consumer_secret"],
+    #                        access_token=config_data["twitter_settings"]["access_token"],
+    #                        access_token_secret=config_data["twitter_settings"]["access_token_secret"])
+    # try:
+    #     tweet_response = client.create_tweet(text="")
+    #     return tweet_response.data
+    # except tweepy.TweepyException as e:
+    #     print(f"Error posting tweet: {e}")
+    #     return None
