cace5a86e4746e3aeab8396cc30ce4fed337a12f
TheGreatCodeHolio <ian@icarey.net>
2024-01-06 01:06:20 -0500

    changed ffmpeg command for opus

--- lib/shell_handler.py
+++ lib/shell_handler.py
@@ -7,23 +7,31 @@ module_logger = logging.getLogger("icad_tone_detection.shell_handler")
 def run_command(command, timeout=None, env=None):
     try:
         with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, env=env) as proc:
-            for output in proc.stdout:
-                module_logger.info(output.strip())
+            try:
+                # Wait for the command to complete
+                stdout, stderr = proc.communicate(timeout=timeout)
 
-            # Print any errors
-            errors = proc.stderr.readlines()
-            for error in errors:
-                module_logger.error(error.strip())
+                # Log the output
+                for line in stdout.splitlines():
+                    module_logger.info(line.strip())
 
+                # Log any errors
+                for line in stderr.splitlines():
+                    module_logger.error(line.strip())
+
+            except subprocess.TimeoutExpired:
+                proc.kill()
+                stdout, stderr = proc.communicate()
+                module_logger.error(f"Command '{' '.join(command)}' timed out after {timeout} seconds.")
+                return False
+
+            # Check the return code and log if there was an error
             if proc.returncode != 0:
                 module_logger.warning(f"Command '{' '.join(command)}' exited with error code {proc.returncode}")
                 return False  # Return False on non-zero return code
 
             return True  # Return True on success
 
-    except subprocess.TimeoutExpired:
-        module_logger.error(f"Command '{' '.join(command)}' timed out after {timeout} seconds.")
-        return False
     except Exception as e:
         module_logger.error(f"An error occurred while running '{' '.join(command)}': {str(e)}")
         return False
