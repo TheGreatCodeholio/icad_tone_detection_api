f7a535cc0527df4701e990f866bb3050c60673ff
TheGreatCodeHolio <ian@icarey.net>
2024-01-06 01:03:36 -0500

    added more logging and fixed a logging bug

--- lib/audio_file_handler.py
+++ lib/audio_file_handler.py
@@ -413,8 +413,8 @@ def convert_mp3_opus(local_audio_path):
     module_logger.warning(local_audio_path)
     module_logger.warning(ogg_file)
 
-    command = ['ffmpeg', '-y', '-i', f'"{local_audio_path}"', '-ac', '1', '-map', '0:a', '-strict', '-2', '-codec:a',
-               'opus', '-b:a', '128k', f'"{ogg_file}"']
+    command = ['ffmpeg', '-y', '-i', f'{local_audio_path}', '-ac', '1', '-map', '0:a', '-strict', '-2', '-codec:a',
+               'opus', '-b:a', '128k', f'{ogg_file}']
 
     ogg_result = run_command(command, timeout=600)
     module_logger.warning(ogg_result)
